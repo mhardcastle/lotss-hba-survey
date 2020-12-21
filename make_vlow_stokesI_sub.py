95f0a89a74e9325d53832bff2887247872df6838
Martin Hardcastle <mjh@extragalactic.info>
2020-12-21 17:15:10 +0000

    vlow updates

--- make_vlow_stokesI_sub.py
+++ make_vlow_stokesI_sub.py
@@ -8,6 +8,15 @@ import os
 import glob
 from auxcodes import run
 from make_vlow_stokesI import update_status,die
+from upload import myglob, do_rsync, images
+
+def do_upload(field):
+    basedir='/beegfs/car/mjh/vlow'
+    workdir=basedir+'/'+field
+    f=myglob('*.archive0',workdir)
+    f+=images('image_full_vlow_nocut_m',workdir)
+    f+=['WSCLEAN_low-MFS-image-int.fits','WSCLEAN_low-MFS-image.fits']
+    do_rsync(field,basedir,f)
 
 def subtract_sources():
     update_status(None,'Subtracting')
@@ -24,17 +33,27 @@ def run_wsclean(useIDG=False,stringMultiscaleScales = "0,4,8,16,32,64,128,256", 
     stringMSs=' '.join(g)
     print("Imaging with multiscale CLEAN (at low-resolution)...")
     if (useIDG):
-        # Generate IDG configuration file. Note that we need 'directoryPointingOwn' here.
+        # Generate IDG configuration file.
         print("Generating IDG configuration file...")
-        with open(directoryPointingOwn + "aconfig.txt", "w") as configurationFile:
+        with open("aconfig.txt", "w") as configurationFile:
             configurationFile.write("aterms=[beam]\nbeam.differential = true\nbeam.update_interval = 600\nbeam.usechannelfreq = true")
 
-        command = "wsclean -no-update-model-required -size " + str(sizePixels) + " " + str(sizePixels) + " -reorder -weight briggs -0.5 -weighting-rank-filter 3 -clean-border 1 -mgain 0.8 -no-fit-beam -data-column DATA -join-channels -channels-out " + str(numberOfSubbands) + " -padding 1.2 -multiscale -multiscale-scales " + stringMultiscaleScalesIDG + " -auto-mask 1.5 -auto-threshold 1.4 -taper-gaussian " + str(taperGaussian) + "arcsec -circular-beam -beam-size " + str(beamSize) + "arcsec -pol i -name " + name + " -scale " + str(scale) + "arcsec -niter 100000 -minuv-l " + str(uvDistanceMinLambda) + " -use-idg -idg-mode " + IDGMode + " -aterm-kernel-size 16 -aterm-config aconfig.txt " + stringMSs
+        command = "wsclean -no-update-model-required -size " + str(sizePixels) + " " + str(sizePixels) + " -reorder -weight briggs -0.5 -weighting-rank-filter 3 -clean-border 1 -mgain 0.8 -no-fit-beam -data-column DATA -join-channels -channels-out " + str(numberOfSubbands) + " -padding 1.2 -multiscale -multiscale-scales " + stringMultiscaleScalesIDG + " -auto-mask 3.0 -auto-threshold 2.5 -taper-gaussian " + str(taperGaussian) + "arcsec -circular-beam -beam-size " + str(beamsize) + "arcsec -pol i -name " + name + " -scale " + str(scale) + "arcsec -niter 100000 -minuv-l " + str(uvDistanceMinLambda) + " -use-idg -idg-mode " + IDGMode + " -aterm-kernel-size 16 -aterm-config aconfig.txt " + stringMSs
     else:
-        command = "wsclean -no-update-model-required -apply-primary-beam -size " + str(sizePixels) + " " + str(sizePixels) + " -reorder -weight briggs -0.5 -weighting-rank-filter 3 -clean-border 1 -mgain 0.8 -fit-beam -data-column DATA -join-channels -channels-out " + str(numberOfSubbands) + " -padding 1.2 -multiscale -multiscale-scales " + stringMultiscaleScales + " -auto-mask 3.0 -auto-threshold 2.5 -taper-gaussian " + str(taperGaussian) + "arcsec -pol i -name " + name + " -scale " + str(scale) + "arcsec -niter 100000 -minuv-l " + str(uvDistanceMinLambda) + " -baseline-averaging 10.0 " + stringMSs
+        command = "wsclean -no-update-model-required -apply-primary-beam -use-differential-lofar-beam -size " + str(sizePixels) + " " + str(sizePixels) + " -reorder -weight briggs -0.5 -weighting-rank-filter 3 -clean-border 1 -mgain 0.8 -no-fit-beam -data-column DATA -join-channels -channels-out " + str(numberOfSubbands) + " -padding 1.2 -multiscale -multiscale-scales " + stringMultiscaleScales + " -auto-mask 3.0 -auto-threshold 2.5 -taper-gaussian " + str(taperGaussian) + "arcsec -circular-beam -beam-size " + str(beamsize) + "arcsec -pol i -name " + name + " -scale " + str(scale) + "arcsec -niter 100000 -minuv-l " + str(uvDistanceMinLambda) + " -baseline-averaging 10.0 " + stringMSs
 
     print(command)
     run(command)
+    print('Now correcting for beam...')
+    
+    f='image_full_vlow_nocut_m.app.restored.fits'
+    dapp=fits.open(f)
+    dint=fits.open(f.replace('app','int'))                     
+    beam=dint[0].data/dapp[0].data                    
+    wsuc=fits.open('WSCLEAN_low-MFS-image.fits')
+    wsuc[0].data*=beam[0,0,12:-13,12:-13]                 
+    wsuc.write('WSCLEAN_low-MFS-image-int.fits')  
+
     update_status(None,'WSCLEAN complete')
     
 if __name__=='__main__':
@@ -46,6 +65,10 @@ if __name__=='__main__':
         subtract_sources()
     elif sys.argv[1]=='clean':
         run_wsclean()
+    elif sys.argv[1]=='cleanidg':
+        run_wsclean(useIDG=True,name='WSCLEAN_IDG')
+    elif sys.argv[1]=='upload':
+        do_upload(field)
     else:
         raise RuntimeError('Failed to parse command')
     
