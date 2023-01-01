57b8cc5669dd226ca25ae93f5fa20d26bf193e53
Martin Hardcastle <mjh@extragalactic.info>
2023-01-01 22:55:01 +0000

    add . option

--- remove_archived.py
+++ remove_archived.py
@@ -24,6 +24,10 @@ if len(sys.argv)>2:
 else:
     selected=None
 
+for i in range(len(selected)):
+    if selected[i]=='.':
+        selected[i]=os.getcwd().split('/')[-1]
+    
 deleted=[]
 for r in results:
     location=r['location']
