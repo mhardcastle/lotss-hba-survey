0a8c044b667fd7bf73e6630117598af2f1e50841
Leah Morabito <lmorabit@gmail.com>
2023-01-17 14:57:08 +0000

    tuesday changes

--- monitor_lbcals.py
+++ monitor_lbcals.py
@@ -5,21 +5,21 @@
 from __future__ import print_function
 from time import sleep
 import datetime
-from surveys_db import SurveysDB, tag_field
+from surveys_db import SurveysDB, tag_field, get_cluster
 
 ## need to update this
-from run_full_field_reprocessing_pipeline import stage_field
+#from run_full_field_reprocessing_pipeline import stage_field
 ## from reprocessing_utils import prepare_field  ## ddf-pipeline utils
 import os
 import threading
 import glob
 import requests
-import staging_access
+import stager_access
 
 
 #################################
 ## CLUSTER SPECIFICS - config file
-cluster = 'cosma'
+cluster = 'cosma'  ## but set as environment variable DDF_PIPELINE_CLUSTER
 basedir = '/cosma5/data/durham/dc-mora2/surveys/'
 
 
@@ -43,106 +43,14 @@ get_lbcalibrator
 set_lbcalibrator
 '''
 
-def stage_cal( srmfilename, srmpath='https://public.spider.surfsara.nl/project/lofarvlbi/srmlists/' ):
+def stage_cal( id, srmpath='https://public.spider.surfsara.nl/project/lofarvlbi/srmlists/' ):
+    srmfilename = id + '_srms.txt'
     response = requests.get(os.path.join(srmpath,srmfilename))
     data = response.text
     uris = data.rstrip('\n').split('\n')
     stage_id = stager_access.stage(uris)
-    update_status(srmfilename.replace('_srms.txt',''), 'Staging', stage_id=stage_id )
+    update_status(id, 'Staging', stage_id=stage_id )
     return stage_id
-
-def check_stage( stage_id, srmfilename ):
-    ## there is actually a function in the stager_access module to do this ... 
-    stager_access.get_progress()
-get_surls_pending
-
-    status = 'new'
-    srmfile = open(srmfilename)
-    uris = srmfile.read().splitlines()
-    while 'success' not in status:
-        status = sa.get_status(stage_id)
-        numonline = sa.get_surls_online(stage_id)
-        print('Status',status,'num total',len(uris),'num staged',len(numonline),srmfilename)
-        if float(len(numonline)) > 0.99*float(len(uris)):
-            break
-        time.sleep(60)
-
-
-
-def do_rclone_download(cname,f,verbose=False):
-    '''
-    Download required data from field cname into location f
-    '''
-    #tarfiles=['images.tar','uv.tar']
-    for macaroon, directory in [('maca_sksp_tape_DR2_readonly.conf',''),('maca_sksp_tape_DDF.conf','archive/'),('maca_sksp_tape_DDF_readonly.conf','other/')]:
-
-
-
-        try:
-            rc=RClone(macaroon,debug=True)
-        except RuntimeError:
-            print('Macaroon',macaroon,'does not exist!')
-            continue
-        rc.get_remote()
-        files=rc.get_files(directory+cname)
-        tarfiles=[fl for fl in files if 'images' in fl or 'uv' in fl]
-        if tarfiles:
-            d=rc.multicopy(rc.remote+directory+cname,tarfiles,f)
-            if d['err'] or d['code']!=0:
-                continue
-        else:
-            continue
-        break
-        
-    else:
-        raise RuntimeError('Failed to download from any source')
-    tarfiles = glob.glob('*tar')
-    untar(f,tarfiles,verbose=verbose)
-
-
-
-        # Format of lines is like: srm://srm.grid.sara.nl:8443/pnfs/grid.sara.nl/data/lofar/ops/projects/lt10_010/775619/L775619_SB055_uv.MS_76a2361e.tar
-        startdir = os.getcwd()
-        os.chdir(outdir)
-        for line in srmfile:
-                origline = line[:-1]
-                line = origline.split('/')
-                print(line)
-                projectcode = line[-3]
-                obsid = line[-2]
-                fileid = line[-1]
-                if 'psnc' in origline:
-                        location = 'Poznan'
-                if 'juelich' in origline:
-                        location = 'Juelic'
-                if 'sara' in origline:
-                        location = 'Sara'
-                if os.path.exists(fileid.split('_')[0] + '_' + fileid.split('_')[1] + '_'  + fileid.split('_')[2]):
-                        print('Already downloaded  ',fileid)
-                        continue
-                if os.path.exists(fileid.split('_')[0] + '_' + fileid.split('_')[1] + '_'  + fileid.split('_')[2].replace('.MS','_avg.MS')):
-                        print('Already downloaded  and averaged',fileid)
-                        continue
-
-                print('Downloading %s'%fileid)
-                if location != 'Poznan':
-                        print('rclone  --multi-thread-streams 1 --config=/project/lotss/Software/prefactor-operations/macaroons/maca_sksp_LTA.conf copy maca_sksp_LTA:/%s/%s/%s  ./'%(projectcode,obsid,fileid))
-                        os.system('rclone  --multi-thread-streams 1 --config=/project/lotss/Software/prefactor-operations/macaroons/maca_sksp_LTA.conf copy maca_sksp_LTA:/%s/%s/%s  ./'%(projectcode,obsid,fileid))
-                else:
-                        print('singularity run -B /etc/grid-security/certificates,/project /cvmfs/atlas.cern.ch/repo/containers/fs/singularity/x86_64-centos7 gfal-copy gsiftp://gridftp.lofar.psnc.pl:2811/lofar/ops/projects/%s/%s/%s ./'%(projectcode,obsid,fileid))
-                        os.system('singularity run -B /etc/grid-security/certificates,/project /cvmfs/atlas.cern.ch/repo/containers/fs/singularity/x86_64-centos7 gfal-copy gsiftp://gridftp.lofar.psnc.pl:2811/lofar/ops/projects/%s/%s/%s ./'%(projectcode,obsid,fileid))
-
-
-                print('Unpacking %s'%fileid)
-                os.system('tar -xf %s'%fileid)
-                if compress:
-                        msfilename = fileid.split('_')[0] + '_' + fileid.split('_')[1] + '_'  + fileid.split('_')[2]
-                        compress_average(msfilename)
-                os.system('rm %s'%fileid)
-        os.chdir(startdir)
-        return
-
-
 
 def download_cal( id ):
     ## adapted (loosely) from tim's scripts 
@@ -153,22 +61,16 @@ def download_cal( id ):
     ## check the status - this should finish when staging is done
     check_stage( stageid, srmfilename )
     ## now download the data
+    print('Cal download status',cal_download_status)
+    ## stage the data if not and then download
+    if cal_download_status == False:
+        cal_stage_id = stage_data_non_sara(calsrm)
+        print('Checking calibrator %s is staged'%fieldproperties['calib_OBSID'])
+        check_stage_non_sara(calsrm,cal_stage_id)
+        print('Downloading calibrator %s'%fieldproperties['calib_OBSID'])
+        download_data(calsrm,outcaldir,False)
 
 
-
-
-
-def unpack_cal(  ):
-
-
-print('Cal download status',cal_download_status)
-## stage the data if not and then download
-if cal_download_status == False:
-    cal_stage_id = stage_data_non_sara(calsrm)
-    print('Checking calibrator %s is staged'%fieldproperties['calib_OBSID'])
-    check_stage_non_sara(calsrm,cal_stage_id)
-    print('Downloading calibrator %s'%fieldproperties['calib_OBSID'])
-    download_data(calsrm,outcaldir,False)
 
 def update_status(name,status,stage_id=None,time=None,workdir=None,av=None,survey=None):
     # adapted from surveys_db
@@ -186,20 +88,8 @@ def update_status(name,status,stage_id=None,time=None,workdir=None,av=None,surve
         if stage_id is not None:
             idd['staging_id']=stage_id
         sdb.db_set('lb_calibrators',idd)
-    sdb.close()
+    sdb.close()        
 
-def do_download(field):
-    update_status(field'Downloading')
-    success=True
-    try:
-        ## function that downloads, end result is tar files on disk
-        prepare_field(field,basedir+'/'+field,verbose=True,operations=['download'])
-    except RuntimeError:
-        success=False
-    if success:
-        update_status(field,'Downloaded')
-    else:
-        update_status(field,'Download failed')
 
 def do_unpack(field):
     update_status(field,operation,'Unpacking')
@@ -303,23 +193,29 @@ while True:
             print('Stage thread seems to have terminated')
             stage_thread=None
 
-        if d['Staging'] < staginglimit and nextfield is not None and 'Staged' not in d:
-            stage_name=nextfield
-            print('We need to stage a new field (%s)' % stage_name)
-            stage_thread=threading.Thread(target=stage_cal,args=(stage_name,))
-            stage_thread.start()
+        ## at the moment this isn't working because nothing has been started and so d is empty
+        if 'Staging' in d.keys():
+            if d['Staging'] < staginglimit and nextfield is not None and 'Staged' not in d:
+                stage_name=nextfield
+                print('We need to stage a new field (%s)' % stage_name)
+                stage_thread=threading.Thread(target=stage_cal,args=(stage_name,))
+                stage_thread.start()
 
-        if d['Staging'] > 0:
+        ## this won't exist at the start so need some logic to avoid errors in d['Staging'] eg. if 'Staging' in d.keys()
+        if 'Staging' in d.keys():
             ## get the staging ids and then check if they are complete
             ## loop over ids and call database to get staging id
             for s in sid:
-                pending = stager_access.get_surls_pending(s)
-                if len(pending) = 0:
+                stage_status = stager_access.get_status(s)
+                #    “new”, “scheduled”, “in progress”, “aborted”, “failed”, “partial success”, “success”, “on hold” 
+                if stage_status == 'success':
+                    print('Staging for {:s} is complete, updating status'.format(sid[s]))
                     update_status(sid[s],'Staged',stage_id=0)
         
         if ksum<totallimit and 'Staged' in d and download_thread is None:
             download_name=fd['Staged'][0]
             print('We need to download a new file (%s)!' % download_name)
+            ## probably want to pass the staging id here
             download_thread=threading.Thread(target=do_download, args=(download_name,))
             download_thread.start()
 
@@ -332,11 +228,11 @@ while True:
         if 'Unpacked' in d:
             for field in fd['Unpacked']:
                 print('Running a new job',field)
-                update_status(field,operation,"Queued")
+                update_status(field,"Queued")
                 ### will need to change the script
                 command="qsub -v FIELD=%s -N repro-%s /home/mjh/pipeline-master/lotss-hba-survey/torque/dynspec.qsub" % (field, field)
                 if os.system(command):
-                    update_status(field,operation,"Submission failed")
+                    update_status(field,"Submission failed")
 
         ## need to step here where we check that if it's done, the pipeline run was successful. 
 
@@ -353,7 +249,7 @@ while True:
                 command='rm -r '+basedir+'/'+field
                 print('running',command)
                 os.system(command)
-                update_status(field,operation,'Complete')
+                update_status(field,'Complete')
 
         print('\n\n-----------------------------------------------\n\n')
         
