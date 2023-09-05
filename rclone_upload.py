e6fcefcc8c68731175ade67f6cdb7f849185e86a
Martin Hardcastle <mjh@extragalactic.info>
2023-09-05 10:05:29 +0100

    fix symlink issue

--- rclone_upload.py
+++ rclone_upload.py
@@ -203,7 +203,10 @@ def upload_field(name,basedir=None,split_uv=False,skip_fits=False):
     fixsymlinks('DDS3_full',verbose=False)
     fixsymlinks('DDS3_full_slow',stype='merged',verbose=False)
     fixsymlinks('DDS2_full',verbose=False)
-    os.symlink(workdir+'/image_full_low.psf.fits',workdir+'/image_full_low_m.psf.fits')
+    try:
+        os.symlink(workdir+'/image_full_low.psf.fits',workdir+'/image_full_low_m.psf.fits')
+    except:
+        pass
     report('Creating tar files')
     update_status(name,'Creating tars',workdir=workdir)
     m=MyGlob(workdir)
