24a8285284bab2a3ed00ba57bd1678ea539479d6
Martin Hardcastle <mjh@extragalactic.info>
2024-01-24 09:40:16 +0000

    updates to calibrator handling code

--- check_calibrator.py
+++ check_calibrator.py
@@ -2,8 +2,10 @@
 # and do other operations on the LOFAR VLBI calibrators
 
 from __future__ import print_function
-from rclone import RClone
+from rclone import RClone,splitlines
 from surveys_db import SurveysDB
+import os
+import subprocess
 
 def find_calibrators(obsid):
     # Find all the calibrators appropriate for a given obsid by
@@ -30,6 +32,8 @@ def download_field_calibrators(field,wd,verbose=False):
     with SurveysDB(readonly=True) as sdb:
         sdb.execute('select * from observations where field=%s',(field,))
         results=sdb.cur.fetchall()
+        if not results:
+            raise RuntimeError('No observations found for field '+field)
         for r in results:
             obsid=r['id']
             if verbose: print('Doing obsid',obsid)
@@ -41,8 +45,45 @@ def download_field_calibrators(field,wd,verbose=False):
                     dest=wd+'/%i/' % obsid
                     download_calibrator(calid,dest)
                     rd[obsid].append(calid)
+                elif verbose: print('     No processed calibrator found!')
     return rd
-        
+
+def untar_file(tarfile,tmpdir,searchfile,destfile,verbose=False):
+    if not os.path.isdir(tmpdir):
+        os.mkdir(tmpdir)
+    command=['tar','tf',tarfile]
+    proc=subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    (out, err) = proc.communicate()
+    files=splitlines(out)
+    for f in files:
+        if os.path.basename(f)==searchfile:
+            fullpath=f
+            break
+    else:
+        raise RuntimeError('Cannot find file %s in tarfile %s' % (searchfile,tarfile))
+    depth=fullpath.count('/')
+    command='tar --strip-components=%i -C %s -xf %s %s' % (depth,tmpdir,tarfile,fullpath)
+    if verbose: print('running',command)
+    os.system(command)
+    if verbose: print('renaming',tmpdir+'/'+searchfile,'as',destfile)
+    os.rename(tmpdir+'/'+searchfile,destfile)
+    os.rmdir(tmpdir)
+    
+
+def unpack_calibrator_sols(wd,rd,verbose=False):
+    # Unpack the calibrator tar files into the obsid directories and
+    # name them suitably.  wd is the working directory for the
+    # field. rd is the dictionary returned by
+    # download_field_calibrators, i.e. a list of calibrator files for
+    # each obsid
+    for obsid in rd:
+        if verbose: print('Doing obsid',obsid)
+        for cal in rd[obsid]:
+            dest=wd+'/%i/' % obsid
+            tarfile=dest+'%i.tgz' % cal
+            if not os.path.isfile(tarfile):
+                raise RuntimeError('Cannot find the calibrator tar file!')
+            untar_file(tarfile,wd+'/tmp','cal_solutions.h5',dest+'/%i_solutions.h5' % cal,verbose=verbose)
 
 def check_calibrator(calid):
     rc=RClone('*lofarvlbi.conf')
@@ -60,6 +101,7 @@ if __name__=='__main__':
     #print('checking %i: %s' % (calid,result))
     #if result:
     #    download_calibrator(calid,'/beegfs/car/mjh/lb')
-    find_calibrators(619688)
-    
+    #print(find_calibrators(619688))
+    d=download_field_calibrators('P229+65','/beegfs/car/mjh/lb',verbose=True)
+    unpack_calibrator_sols('/beegfs/car/mjh/lb',d,verbose=True)
     
