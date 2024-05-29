76cab2522e36b11b491aa97f54681fc44220e709
lmorabit <lmorabit@gmail.com>
2024-05-29 14:32:49 +0200

    ddf custer options

--- monitor_lbfields.py
+++ monitor_lbfields.py
@@ -242,7 +242,11 @@ while True:
                     print('next task is',next_task)
                     update_status(field,'Queued')
                     fieldobsid = '{:s}/{:s}'.format(field,obsid)
-                    command = "sbatch -J {:s} {:s} {:s}/lotss-hba-survey/slurm/run_{:s}.sh {:s}".format(field, os.getenv('CLUSTER_OPTS'), str(softwaredir).rstrip('/'), next_task, fieldobsid)
+                    if 'ddf' in next_task:
+                        cluster_opts = os.getenv('DDF_CLUSTER_OPTS')
+                    else:
+                        cluster_opts = os.getenv('CLUSTER_OPTS')
+                    command = "sbatch -J {:s} {:s} {:s}/lotss-hba-survey/slurm/run_{:s}.sh {:s}".format(field, cluster_opts, str(softwaredir).rstrip('/'), next_task, fieldobsid)
                     ## will need run scripts for each task
                     if os.system(command):
                         update_status(field,"Submission failed")
