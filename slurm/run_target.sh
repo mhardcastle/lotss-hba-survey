b0c1d1ae6a47ae2aef1424ff4693557af3af652d
lmorabit <lmorabit@gmail.com>
2024-08-28 11:33:55 +0200

    toil for target

--- slurm/run_target.sh
+++ slurm/run_target.sh
@@ -1,10 +1,8 @@
 #!/bin/bash 
 #SBATCH -N 1                  # number of nodes
-#SBATCH -c 32                 # number of cores
+#SBATCH -c 1
 #SBATCH --ntasks=1            # number of tasks
-#SBATCH -t 12:00:00           # maximum run time in [HH:MM:SS] or [MM:SS] or [minutes]
-#SBATCH -p normal             # partition (queue); job can run up to 3 days
-#SBATCH --output=/project/lofarvlbi/Share/surveys/logs/R-%x.%j.out
+#SBATCH -t 24:00:00           # maximum run time in [HH:MM:SS] or [MM:SS] or [minutes]
 
 ## submit the job with OBSID as an argument
 OBSID=${1}
@@ -17,6 +15,11 @@ export LINCDIR=${SOFTWAREDIR}/LINC
 export FLOCSDIR=${SOFTWAREDIR}/flocs
 BINDPATHS=${SOFTWAREDIR},${DATA_DIR}
 
+## FOR TOIL
+export TOIL_SLURM_ARGS="${CLUSTER_OPTS} --export=ALL -t 24:00:00"
+export CWL_SINGULARITY_CACHE=${SOFTWAREDIR}/singularity
+export TOIL_CHECK_ENV=True
+
 #################################################################################
 ## IN GENERAL DO NOT TOUCH ANYTHING BELOW HERE
 
@@ -24,10 +27,15 @@ BINDPATHS=${SOFTWAREDIR},${DATA_DIR}
 DATADIR=${DATA_DIR}/${OBSID}
 PROCDIR=${DATA_DIR}/processing
 OUTDIR=${PROCDIR}/${OBSID}
-TMPDIR=${PROCDIR}/${OBSID}/tmp/
+WORKDIR=${OUTDIR}/workdir
+OUTPUT=${OUTDIR}
+JOBSTORE=${OUTDIR}/jobstore
+TMPD=${OUTDIR}/tmp
 LOGSDIR=${OUTDIR}/logs
-mkdir -p ${TMPDIR}
+mkdir -p ${TMPD}
+mkdir -p ${TMPD}_interim
 mkdir -p ${LOGSDIR}
+mkdir -p ${WORKDIR}
 
 ## location of LINC
 LINC_DATA_ROOT=${LINCDIR}
@@ -35,10 +43,15 @@ LINC_DATA_ROOT=${LINCDIR}
 # Pass along necessary variables to the container.
 CONTAINERSTR=$(singularity --version)
 if [[ "$CONTAINERSTR" == *"apptainer"* ]]; then
+    export APPTAINER_CACHEDIR=${SOFTWAREDIR}/singularity
+    export APPTAINER_TMPDIR=${APPTAINER_CACHEDIR}/tmp
+    export APPTAINER_PULLDIR=${APPTAINER_CACHEDIR}/pull
+    export APPTAINER_BIND=${BINDPATHS}
     export APPTAINERENV_LINC_DATA_ROOT=${LINC_DATA_ROOT}
     export APPTAINERENV_LOGSDIR=${LOGSDIR}
     export APPTAINERENV_TMPDIR=${TMPDIR}
-    export APPTAINERENV_PREPEND_PATH=${LINC_DATA_ROOT}/scripts
+    export SINGULARITYENV_PREPEND_PATH=${LINC_DATA_ROOT}/scripts
+    export APPTAINERENV_PYTHONPATH=${LINC_DATA_ROOT}/scripts:\$PYTHONPATH
 else
     export SINGULARITYENV_LINC_DATA_ROOT=${LINC_DATA_ROOT}
     export SINGULARITYENV_LOGSDIR=${LOGSDIR}
@@ -50,14 +63,15 @@ fi
 cd ${OUTDIR}
 
 ## pipeline input
-singularity exec -B ${PWD},${BINDPATHS} --no-home ${LOFAR_SINGULARITY} python ${FLOCSDIR}/runners/create_ms_list.py LINC target --cal_solutions ${DATADIR}/LINC-cal_solutions.h5 ${DATADIR} > create_mslist.log
+singularity exec -B ${PWD},${BINDPATHS} --no-home ${LOFAR_SINGULARITY} python3 ${FLOCSDIR}/runners/create_ms_list.py LINC target --cal_solutions ${DATADIR}/LINC-cal_solutions.h5 ${DATADIR} > create_mslist.log
 
 echo LINC starting
 TMPID=`echo ${OBSID} | cut -d'/' -f 1`
-echo export PYTHONPATH=\$LINC_DATA_ROOT/scripts:\$PYTHONPATH > tmprunner_${TMPID}.sh
-echo 'cwltool --parallel --preserve-entire-environment --no-container --tmpdir-prefix=${TMPDIR} --outdir=${OUTDIR} --leave-tmpdir --log-dir=${LOGSDIR} ${LINC_DATA_ROOT}/workflows/HBA_target.cwl mslist_LINC_target.json' >> tmprunner_${TMPID}.sh
-(time singularity exec -B ${PWD},${BINDPATHS} --no-home ${LOFAR_SINGULARITY} bash tmprunner_${TMPID}.sh 2>&1) | tee ${OUTDIR}/job_output.txt
-echo LINC ended
+
+ulimit -n 8192
+
+toil-cwl-runner --no-read-only --singularity --bypass-file-store --jobStore=${JOBSTORE} --logFile=${OUTDIR}/job_output.txt --workDir=${WORKDIR} --outdir=${OUTPUT} --retryCount=0 --writeLogsFromAllJobs=True --writeLogs=${LOGSDIR} --tmp-outdir-prefix=${TMPD}/ --coordinationDir=${OUTPUT} --tmpdir-prefix=${TMPD}_interim/ --disableAutoDeployment=True --preserve-environment ${APPTAINERENV_PYTHONPATH} ${SINGULARITYENV_PREPEND_PATH} ${APPTAINERENV_LINC_DATA_ROOT} ${APPTAINER_BIND} ${APPTAINER_PULLDIR} ${APPTAINER_TMPDIR} ${APPTAINER_CACHEDIR} --batchSystem=slurm ${LINC_DATA_ROOT}/workflows/HBA_target.cwl mslist_LINC_target.json
+
 if grep 'Final process status is success' ${OUTDIR}/job_output.txt
 then 
 	echo 'SUCCESS: Pipeline finished successfully' > ${OUTDIR}/finished.txt
