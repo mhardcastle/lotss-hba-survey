30efcffac018062e110335ae225764fae8ebf42e
r-timmerman <26470519+r-timmerman@users.noreply.github.com>
2025-03-27 10:19:32 +0000

    Update run_split-directions.sh
    Now also grabs the LINC_VLBI output

--- slurm/run_split-directions.sh
+++ slurm/run_split-directions.sh
@@ -38,7 +38,7 @@ if test -d ${DATA_DIR}/${OBSID}/process-ddf
 then
         DATADIR=${DATA_DIR}/${OBSID}/process-ddf
 else
-        DATADIR=${DATA_DIR}/${OBSID}/concatenate-flag
+        DATADIR=${DATA_DIR}/${OBSID}/HBA_target_VLBI/results
 fi
 PROCDIR=${DATA_DIR}/processing
 OUTDIR=${PROCDIR}/${CATOUTDIR}
@@ -69,7 +69,7 @@ export APPTAINERENV_PYTHONPATH=${VLBIDIR}/scripts:${LINCDIR}/scripts:\$PYTHONPAT
 cd ${OUTDIR}
 
 ## list of measurement sets 
-apptainer exec -B ${PWD},${BINDPATHS} --no-home ${LOFAR_SINGULARITY} python3 ${FLOCSDIR}/runners/create_ms_list.py VLBI split-directions --max_dp3_threads 8 --h5merger=${LOFARHELPERS} --selfcal=${FACETSELFCAL} --do_selfcal=false --delay_solset ${DELAYSOLS} --image_cat ${IMCAT} --ms_suffix .ms ${DATADIR} >> create_ms_list.log 2>&1
+apptainer exec -B ${PWD},${BINDPATHS} --no-home ${LOFAR_SINGULARITY} python3 ${FLOCSDIR}/runners/create_ms_list.py VLBI split-directions --max_dp3_threads 8 --h5merger=${LOFARHELPERS} --selfcal=${FACETSELFCAL} --do_selfcal=false --delay_solset ${DELAYSOLS} --image_cat ${IMCAT} --ms_suffix .dp3concat ${DATADIR} >> create_ms_list.log 2>&1
 
 echo LINC starting
 TMPID=`echo ${OBSID} | cut -d'/' -f 1`
